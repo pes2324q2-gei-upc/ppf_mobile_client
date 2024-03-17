a9642e726fb44eb6ae5e234bd35f5424a619e982
AndreaFrutos <andrea.frutos@estudiantat.upc.edu>
2024-03-17 17:46:08 +0100

    creación de ramas y pruebas varias

---
+++ lib/Idiom/language.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+return const MaterialApp(
+  title: 'Localizations Sample App',
+  localizationsDelegates: [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales: [
+    Locale('en'), // English
+    Locale('es'), // Spanish
+  ],
+  home: MyHomePage(),
+);
