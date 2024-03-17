4b18e51409a204abd58c71f62b48f885aa2ead08
Mikigb77 <miquelgarcia.alu@gmail.com>
2024-03-17 16:26:57 +0100

    update with initial camera in bcn

--- lib/views/map_preview_screen.dart
+++ lib/views/map_preview_screen.dart
@@ -15,7 +15,7 @@ class MapSampleState extends State<MapSample> {
       Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(41.38668599929463, 2.196905132696342),
     zoom: 14.4746,
   );
 
